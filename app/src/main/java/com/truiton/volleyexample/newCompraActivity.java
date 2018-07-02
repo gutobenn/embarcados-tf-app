edc8aa5279369b97a5f942aea06f8d3fa5eb46ed
Augusto Bennemann <gutobenn@gmail.com>
2018-07-01 23:29:18 -0300

    Loading spinner on new compra

--- app/src/main/java/com/truiton/volleyexample/newCompraActivity.java
+++ app/src/main/java/com/truiton/volleyexample/newCompraActivity.java
@@ -1,5 +1,8 @@
 package com.truiton.volleyexample;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
@@ -7,6 +10,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.icu.util.Calendar;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -54,6 +58,8 @@ public class newCompraActivity extends AppCompatActivity {
     private RequestQueue mQueue;
     private Button botao;
     private Bitmap bitmap;
+    private View mProgressView;
+    private View mFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,6 +83,8 @@ public class newCompraActivity extends AppCompatActivity {
         mAddress = (TextView) findViewById(R.id.addressField);
         mButton = (Button) findViewById(R.id.submitButton);
         mTextView = (TextView) findViewById(R.id.result);
+        mProgressView = findViewById(R.id.submit_progress);
+        mFormView = findViewById(R.id.form);
 
         bitmap = null;
     }
@@ -112,6 +120,8 @@ public class newCompraActivity extends AppCompatActivity {
             String url = "https://ccapi.florescer.xyz/api/v1/compras/";
             JSONObject jsonParams = new JSONObject();
 
+            showProgress(true);
+
             try {
                 if (bitmap != null) {
                     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -134,6 +144,7 @@ public class newCompraActivity extends AppCompatActivity {
             CustomJSONObjectRequest sr = new CustomJSONObjectRequest(Request.Method.POST, url, jsonParams, new Response.Listener<JSONObject>() {
                 @Override
                 public void onResponse(JSONObject response) {
+                    showProgress(false);
                     try {
                         Intent intent = new Intent(newCompraActivity.this, ViewCompraActivity.class);
                         intent.putExtra(ID_TO_VIEW_MSG, ((JSONObject) response).getJSONObject("data").getString("id"));
@@ -146,6 +157,7 @@ public class newCompraActivity extends AppCompatActivity {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
+                    showProgress(false);
                     //mTextView.setText("Erro ao cadastrar");
                     Toast.makeText(newCompraActivity.this, "Erro ao cadastrar!", Toast.LENGTH_SHORT).show();
                 }
@@ -209,4 +221,40 @@ public class newCompraActivity extends AppCompatActivity {
                             .show();
                 }
             };
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
 }
